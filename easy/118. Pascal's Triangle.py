c352706f31013764465ae5cb4ef5d436ce5f121b
matyokubov <accf9883@gmail.com>
2024-04-03 10:30:38 +0500

    commit

---
+++ easy/118. Pascal's Triangle.py
@@ -0,0 +1,16 @@
+#Input
+numRows = 5
+
+#Logic
+def main(numRows):
+    res = [[1]]
+    for _ in range(numRows-1): # identify how much it cycles
+        temp = [0]+res[-1]+[0]
+        row = []
+        for j in range(len(res[-1])+1): # identify next row's length
+            row.append(temp[j]+temp[j+1])
+        res.append(row)
+    return res
+    
+#Output
+print(main(numRows))
